d8b1d26e42e3071b004a5e3ccbf6f81b4ae83dde
Darrian Soto <ddog350@gmail.com>
2019-10-12 15:34:56 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(lawyer)
+    @lawyer = lawyer
+  end
+  
+  def job
+    @lawyer
+  end
+end
