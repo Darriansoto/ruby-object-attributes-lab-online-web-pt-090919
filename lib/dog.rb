88ee3e4b685dfc07481f8085e0002fcab0fb8428
Darrian Soto <ddog350@gmail.com>
2019-10-12 15:33:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(husky)
+    @husky = husky
+  end
+  
+  def breed
+    @husky
+  end
+end
